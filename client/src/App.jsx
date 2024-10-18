ca86d81f3ca1a5a62293684d952a6db191fe9da6
omkar kore <142180062+Omkore-09@users.noreply.github.com>
2024-10-18 12:49:15 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -68,8 +68,7 @@ function App() {
             </CheckAuth>
           }
         >
-          <Route path="home" element={<ShoppingHome />} />
-          <Route path="listing" element={<ShoppingListing />} />
+         
           <Route path="checkout" element={<ShoppingCheckout />} />
           <Route path="account" element={<ShoppingAccount />} />
         </Route>
