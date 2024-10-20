e8b7aa48817d19234e5e612448e03aac06888df5
omkar kore <142180062+Omkore-09@users.noreply.github.com>
2024-10-20 12:04:54 +0530

    Update Home.jsx

--- client/src/pages/shopping-view/Home.jsx
+++ client/src/pages/shopping-view/Home.jsx
@@ -1,9 +1,8 @@
 import { HousePlug, LogOut, Menu, ShoppingCart, UserCog, Vegan } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { Sheet, SheetContent, SheetTrigger } from "../../components/ui/sheet";
-
-import { Button } from "../../components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
 
 import { useDispatch, useSelector } from "react-redux";
 import { shoppingViewHeaderMenuItems } from "@/config/Index";
@@ -12,32 +11,48 @@ import { Avatar, AvatarFallback } from "../ui/avatar";
 import { logoutUser } from "@/store/auth-slice";
 import UserCartWrapper from "./cart-wrapper";
 import { fetchCartItems } from "@/store/shop/cart-slice";
-import logo from '../../assets/logobg.png';
+import { Label } from "../ui/label";
+import logo from '../../assets/logobg.png'
 
 function MenuItems() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   function handleNavigate(getCurrentMenuItem) {
     sessionStorage.removeItem("filters");
     const currentFilter =
-      getCurrentMenuItem.id !== "home" && getCurrentMenuItem.id !== "products" && getCurrentMenuItem.id !== "search"
-        ? { category: [getCurrentMenuItem.id] }
+      getCurrentMenuItem.id !== "home" &&
+      getCurrentMenuItem.id !== "products" &&
+      getCurrentMenuItem.id !== "search"
+        ? {
+            category: [getCurrentMenuItem.id],
+          }
         : null;
+
     sessionStorage.setItem("filters", JSON.stringify(currentFilter));
-    navigate(getCurrentMenuItem.path);
+
+    location.pathname.includes("listing") && currentFilter !== null
+      ? setSearchParams(
+          new URLSearchParams(`?category=${getCurrentMenuItem.id}`)
+        )
+      : navigate(getCurrentMenuItem.path);
   }
+
+
+
+
 
   return (
     <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
       {shoppingViewHeaderMenuItems.map((menuItem) => (
         <Link
-          className="text-sm font-medium"
-          key={menuItem.id}
-          to={menuItem.path}
-          onClick={() => handleNavigate(menuItem)}
-        >
-          {menuItem.label}
-        </Link>
+        className="text-sm font-medium"
+        key={menuItem.id}
+        to={menuItem.path}
+        onClick={() => handleNavigate(menuItem)}
+      >
+        {menuItem.label}
+      </Link>
       ))}
     </nav>
   );
@@ -111,7 +126,7 @@ const ShoppingHeader = () => {
     <header className="fixed top-0 z-40 w-full border-b bg-background bg-green-700">
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
         <Link to="/shop/home" className="flex items-center gap-2">
-          <img src={logo} alt="Ecommerce Logo" className="h-8 w-10" />
+        <img src={logo} alt="Ecommerce Logo" className="h-8 w-10" />
           <span className="font-bold text-white">Adivasi Herbal Hair Oil</span>
         </Link>
         <Sheet>
