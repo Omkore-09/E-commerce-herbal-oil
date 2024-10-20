f43075edba42836994443fd2ddbfa8e4448f1401
omkar kore <142180062+Omkore-09@users.noreply.github.com>
2024-10-20 12:09:58 +0530

    Update Header.jsx

--- client/src/components/shopping-view/Header.jsx
+++ client/src/components/shopping-view/Header.jsx
@@ -1,61 +1,15 @@
 import { HousePlug, LogOut, Menu, ShoppingCart, UserCog, Vegan } from "lucide-react";
 import React, { useEffect, useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { Button } from "../ui/button";
 import { useDispatch, useSelector } from "react-redux";
-import { shoppingViewHeaderMenuItems } from "@/config/Index";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import { logoutUser } from "@/store/auth-slice";
 import UserCartWrapper from "./cart-wrapper";
 import { fetchCartItems } from "@/store/shop/cart-slice";
-import { Label } from "../ui/label";
-import logo from '../../assets/logobg.png'
-
-function MenuItems() {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  function handleNavigate(getCurrentMenuItem) {
-    sessionStorage.removeItem("filters");
-    const currentFilter =
-      getCurrentMenuItem.id !== "home" &&
-      getCurrentMenuItem.id !== "products" &&
-      getCurrentMenuItem.id !== "search"
-        ? {
-            category: [getCurrentMenuItem.id],
-          }
-        : null;
-
-    sessionStorage.setItem("filters", JSON.stringify(currentFilter));
-
-    location.pathname.includes("listing") && currentFilter !== null
-      ? setSearchParams(
-          new URLSearchParams(`?category=${getCurrentMenuItem.id}`)
-        )
-      : navigate(getCurrentMenuItem.path);
-  }
-
-
-
-
-
-  return (
-    <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
-      {shoppingViewHeaderMenuItems.map((menuItem) => (
-        <Link
-        className="text-sm font-medium"
-        key={menuItem.id}
-        to={menuItem.path}
-        onClick={() => handleNavigate(menuItem)}
-      >
-        {menuItem.label}
-      </Link>
-      ))}
-    </nav>
-  );
-}
+import logo from '../../assets/logobg.png';
 
 function HeaderRightContent() {
   const { user, isAuthenticated } = useSelector((state) => state.auth);
@@ -125,7 +79,7 @@ const ShoppingHeader = () => {
     <header className="fixed top-0 z-40 w-full border-b bg-background bg-green-700">
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
         <Link to="/shop/home" className="flex items-center gap-2">
-        <img src={logo} alt="Ecommerce Logo" className="h-8 w-10" />
+          <img src={logo} alt="Ecommerce Logo" className="h-8 w-10" />
           <span className="font-bold text-white">Adivasi Herbal Hair Oil</span>
         </Link>
         <Sheet>
@@ -136,12 +90,26 @@ const ShoppingHeader = () => {
             </Button>
           </SheetTrigger>
           <SheetContent side="left" className="w-full max-w-xs">
-            <MenuItems />
+            <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+              <Link className="text-sm font-medium" to="/home">Home</Link>
+              <Link className="text-sm font-medium" to="/listing">Listing</Link>
+              <Link className="text-sm font-medium" to="/aboutus">About Us</Link>
+              <Link className="text-sm font-medium" to="/product-benefits">Product Benefits</Link>
+              <Link className="text-sm font-medium" to="/ingredients">Ingredients</Link>
+              <Link className="text-sm font-medium" to="/contact-us">Contact Us</Link>
+            </nav>
             <HeaderRightContent />
           </SheetContent>
         </Sheet>
         <div className="hidden lg:block text-white text-2xl">
-          <MenuItems />
+          <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+            <Link className="text-sm font-medium" to="/home">Home</Link>
+            <Link className="text-sm font-medium" to="/listing">Listing</Link>
+            <Link className="text-sm font-medium" to="/aboutus">About Us</Link>
+            <Link className="text-sm font-medium" to="/product-benefits">Product Benefits</Link>
+            <Link className="text-sm font-medium" to="/ingredients">Ingredients</Link>
+            <Link className="text-sm font-medium" to="/contact-us">Contact Us</Link>
+          </nav>
         </div>
         <div className="hidden lg:block text-green-700">
           <HeaderRightContent />
