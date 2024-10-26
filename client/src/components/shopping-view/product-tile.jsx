d710e5244f9a345d63262a309928343bfd408477
omkar kore <142180062+Omkore-09@users.noreply.github.com>
2024-10-26 17:28:47 +0530

    Update product-tile.jsx

--- client/src/components/shopping-view/product-tile.jsx
+++ client/src/components/shopping-view/product-tile.jsx
@@ -3,15 +3,15 @@ import { Card, CardContent, CardFooter } from "../ui/card";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
 
-const ShoppingProductTile = ({ product , handleGetProductDetails , handleAddtoCart }) => {
+const ShoppingProductTile = ({ product, handleGetProductDetails, handleAddtoCart }) => {
   return (
     <Card className="w-full max-w-sm mx-auto">
-      <div onClick={()=>handleGetProductDetails(product?._id)}>
+      <div onClick={() => handleGetProductDetails(product?._id)}>
         <div className="relative">
           <img
             src={product?.image}
             alt={product.title}
-            className="w-full h-[300px] object-cover rounded-t-lg"
+            className="w-full object-contain rounded-t-lg"
           />
           {product?.salePrice > 0 ? (
             <Badge className="absolute top-2 left-2 bg-red-500 hover:bg-red-600">
@@ -20,8 +20,7 @@ const ShoppingProductTile = ({ product , handleGetProductDetails , handleAddtoCa
           ) : null}
         </div>
         <CardContent className="p-4">
-          <h2 className="text-xl font-bold mb-2">{product?.title} </h2>
-
+          <h2 className="text-xl font-bold mb-2">{product?.title}</h2>
           <div className="flex justify-between items-center mb-2">
             <span
               className={`${
@@ -37,14 +36,12 @@ const ShoppingProductTile = ({ product , handleGetProductDetails , handleAddtoCa
             ) : null}
           </div>
         </CardContent>
-
-       
       </div>
       <CardFooter>
-            <Button onClick={()=>handleAddtoCart(product?._id)} className="w-full">
-                Add to cart
-            </Button>
-        </CardFooter>
+        <Button onClick={() => handleAddtoCart(product?._id)} className="w-full">
+          Add to cart
+        </Button>
+      </CardFooter>
     </Card>
   );
 };
