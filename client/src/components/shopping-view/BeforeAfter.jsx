625d9f5aafbccd59ccd13a5d1b069feb71c3c1f5
omkar kore <142180062+Omkore-09@users.noreply.github.com>
2025-05-01 21:45:05 +0530

    Update BeforeAfter.jsx

--- client/src/components/shopping-view/BeforeAfter.jsx
+++ client/src/components/shopping-view/BeforeAfter.jsx
@@ -9,7 +9,7 @@ import Customer6 from '../../assets/ba7.jpg'
 import Customer7 from '../../assets/ba8.jpg'
 import Customer8 from '../../assets/ba9.jpg'
 import Customer9 from '../../assets/ba10.jpg'
-import Customer10 from '../../assets/ba10.jpg'
+// import Customer10 from '../../assets/ba10.jpg'
 
 const images = [
   {
@@ -57,11 +57,7 @@ const images = [
     name: 'Customer 9',
     problem: ' '
   },
-  {
-    src: Customer10,
-    name: 'Customer 10',
-    problem: ' '
-  },
+  
   
 ];
 
